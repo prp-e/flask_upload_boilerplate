1085cb66cb7abd1082299520466eee625e21c5c2
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-10-15 18:31:41 +0330

    Main API

---
+++ app.py
@@ -0,0 +1,20 @@
+import os 
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html', filename=filename)
+            
+    return render_template('index.html')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
